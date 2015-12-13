b82490ff36a0522902607315d387d5992c0cf385
masa11217a <il.avril.song.m.i.wlcaj@gmail.com>
2015-12-13 22:07:33 +0900

    initial commit

--- app/src/main/java/com/example/student001/sc2/MainActivity.java
+++ app/src/main/java/com/example/student001/sc2/MainActivity.java
@@ -1,168 +1,100 @@
 package com.example.student001.sc2;
 
-import android.app.Activity;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+import android.preference.DialogPreference;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.kii.cloud.storage.Kii;
+
+import java.security.Permissions;
+import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+public class MainActivity extends AppCompatActivity {
 
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
-
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
+    private KiiManager kiiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Intent top = new Intent(this, Top.class);
+        startActivityForResult(top, 0);
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
-
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
-    }
-
-
-
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
-
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 2:
-                mTitle = getString(R.string.title_section1);
-               Intent top = new Intent(this, Top.class);
-                startActivityForResult(top, 0);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section2);
-                Intent news = new Intent(this, News.class);
-                startActivityForResult(news, 0);
-                break;
-            case 4:
-                mTitle = getString(R.string.title_section3);
-                Intent access = new Intent(this, Access.class);
-                startActivityForResult(access, 0);
-                break;
-            case 5:
-                mTitle = getString(R.string.title_section4);
-                Intent schedule = new Intent(this, Schedule.class);
-                startActivityForResult(schedule, 0);
-                break;
-            case 6:
-                mTitle = getString(R.string.title_section5);
-                Intent mail = new Intent(this, Mail.class);
-                startActivityForResult(mail, 0);
-                break;
-            case 7:
-                mTitle = getString(R.string.title_section6);
-                Intent map = new Intent(this, Map.class);
-                startActivityForResult(map, 0);
-                break;
+        // Android6.0+ はパーミッションの要求が必要
+        boolean execReqPermissions = requestAppPermissions();
+        if(!execReqPermissions) {
+            //　パーミッションが認証済みなのでログイン開始
+            loginWithStoredCredentials();
         }
     }
 
+    //　戻り値はリクエストを実行したか
+    private boolean requestAppPermissions() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return false;
+        }
 
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
+        ArrayList<String> reqList = new ArrayList<>();
 
+        PackageManager pm = getPackageManager();
+        int checkPermission = pm.checkPermission(Manifest.permission.INTERNET, getPackageName());
+        if (checkPermission == PackageManager.PERMISSION_DENIED) {
+            reqList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
+        if (reqList.size() <= 0) {
             return true;
         }
-        return super.onCreateOptionsMenu(menu);
+        requestPermissions(reqList.toArray(new String[reqList.size()]), 0);
+        return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.top, container, false);
-            return rootView;
+        public void onRequestPermissionsResult( int requestCode, String[] permissions, int[] grantResults) {
+            for (int grantResult : grantResults) {
+                if(grantResult == PackageManager.PERMISSION_DENIED) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("エラー");
+                    builder.setMessage("許可しなければアプリは動きません");
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            requestAppPermissions();
+                        }
+                    });
+                    builder.setCancelable(false);
+                    builder.show();
+                    return;
+                }
+            }
+
+            // 承認が得られたらログインを開始
+            loginWithStoredCredentials();
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
 
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
+        private void loginWithStoredCredentials() {
+            kiiManager = KiiManager.getInstance(this);
+            kiiManager.loginWithStoredCredentials(new KiiManager.OnFinishActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        Log.d("loginWithSC", "success");
+                        // TODO データのやり取りのサンプル
+                    }
+
+                    @Override
+                    public void onFail(Exception e){
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                    }
+            });
         }
-    }
+
 }
